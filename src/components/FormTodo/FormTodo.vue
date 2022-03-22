ef1628a9af77e10de1be8e4840fc7b511f06f57e
renab <renaudabadie@gmail.com>
2022-03-22 16:25:06 +0100

    Component FormTodoActions

--- src/components/FormTodo/FormTodo.vue
+++ src/components/FormTodo/FormTodo.vue
@@ -2,22 +2,10 @@
   <form-todo-header>Ajouter un Todo</form-todo-header>
   <q-form @submit="addTodo" class="q-gutter-md rounded-borders">
     <div class="q-pa-md">
-
       <form-todo-label v-model="todo.label"/>
       <form-todo-duedate v-model="todo.dueDate"/>
-
       <q-checkbox left-label v-model="todo.done" label="Terminé" />
-      <div>
-        <q-btn-group spread unelevated rounded  >
-          <q-btn type="submit" color="primary" label="Ajouter" class="q-ma-md" />
-          <q-btn
-            @click="$emit('cancel')"
-            color="secondary"
-            label="Annuler"
-            class="q-ma-md"
-          />
-        </q-btn-group>
-      </div>
+      <form-todo-actions @cancel="$emit('cancel')"/>
     </div>
   </q-form>
 </template>
@@ -28,17 +16,18 @@ import { uid } from 'quasar'
 import FormTodoHeader from './FormTodoHeader'
 import FormTodoLabel from './FormTodoLabel'
 import FormTodoDueDate from './FormTodoDueDate'
+import FormTodoActions from './FormTodoActions'
 
 export default defineComponent({
   name: 'form-todo',
   components: {
     'form-todo-header': FormTodoHeader,
     'form-todo-label': FormTodoLabel,
-    'form-todo-duedate': FormTodoDueDate
+    'form-todo-duedate': FormTodoDueDate,
+    'form-todo-actions': FormTodoActions
   },
   data () {
     return {
-      test: 'caca',
       todo: {
         id: uid(),
         label: '',
---
+++ src/components/FormTodo/FormTodoActions.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+  <q-btn-group spread unelevated rounded  >
+      <q-btn type="submit" color="primary" label="Ajouter" class="q-ma-md" />
+      <q-btn
+        @click="$emit('cancel')"
+        color="secondary"
+        label="Annuler"
+        class="q-ma-md"
+      />
+  </q-btn-group>
+</div>
+</template>
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  methods: {
+  },
+  emits: ['cancel']
+})
+</script>
