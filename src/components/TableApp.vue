2e42af189299e35f1786ba96e32e489dcbf2adcf
renab <renaudabadie@gmail.com>
2022-03-23 16:48:00 +0100

    test component qtable

---
+++ src/components/TableApp.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="q-pa-md">
+    <q-table
+      color="primary"
+      title="Treats"
+      :rows="rows"
+      :columns="columns"
+      row-key="id"
+      :loading="loading"
+    />
+  </div>
+</template>
+
+<script>
+const columns = [
+  { name: 'id', align: 'left', label: 'id', field: 'id', sortable: true },
+  { name: 'label', label: 'Libellé', align: 'left', field: 'label', sortable: true },
+  { name: 'date', label: 'Date', field: 'date', align: 'right', sortable: true }
+]
+
+export default {
+  props: ['rows', 'loading'],
+  setup () {
+    return {
+      columns
+    }
+  }
+}
+</script>
--- src/pages/PageListTodosWithAxios.vue
+++ src/pages/PageListTodosWithAxios.vue
@@ -2,19 +2,32 @@
   <q-page class="q-pa-lg flex flex-start column">
       <div class="row">
         <h4>Mes Todos</h4>
-        <q-btn @click="this.showDialogAddTodo = true" color="primary" label="Ajouter" class="center q-ma-xl" />
       </div>
-      <div class="q-ma-md">
-        <todo-list-not-done :loading="loading" :todosNotDone="todosNotDone"/>
-      </div>
-      <div class="q-ma-md">
-        <todo-list-done :loading="loading" :todosDone="todosDone"/>
-      </div>
-      <q-dialog v-model="showDialogAddTodo">
-        <q-card class="q-pa-xl">
-          <todo-form-add  @cancel="this.showDialogAddTodo = false" />
-        </q-card>
-      </q-dialog>
+      <q-btn
+        @click="this.showTablesModeList = !this.showTablesModeList"
+        color="secondary"
+        class="q-ma-md" label="Toggle résultats mode liste"/>
+
+      <div v-if="showTablesModeList">
+        <div class="q-ma-md">
+          <todo-list-not-done :loading="loading" :todosNotDone="todosNotDone"/>
+        </div>
+        <div class="q-ma-md">
+          <todo-list-done :loading="loading" :todosDone="todosDone"/>
+        </div>
+        <q-dialog v-model="showDialogAddTodo">
+          <q-card class="q-pa-xl">
+            <todo-form-add  @cancel="this.showDialogAddTodo = false" />
+          </q-card>
+        </q-dialog>
+       </div>
+       <q-btn
+        @click="this.showTablesModeTable = !this.showTablesModeTable"
+        color="secondary"
+        class="q-ma-md" label="Toggle résultats mode table"/>
+       <div v-if="showTablesModeTable">
+        <table-app :rows="todosDone" :loading="loading" />
+       </div>
   </q-page>
 
 </template>
@@ -26,6 +39,7 @@ import FormTodoAdd from '../Components/FormTodo/FormTodoAdd'
 import ListTodosNotDone from '../Components/ListTodo/ListTodosNotDone'
 
 import axios from 'axios'
+import TableApp from '../Components/TableApp'
 
 export default defineComponent({
   name: 'ListTodos',
@@ -34,13 +48,16 @@ export default defineComponent({
       todosDone: [],
       todosNotDone: [],
       loading: true,
-      showDialogAddTodo: false
+      showDialogAddTodo: false,
+      showTablesModeList: false,
+      showTablesModeTable: true
     }
   },
   components: {
     'todo-list-done': ListTodosDone,
     'todo-list-not-done': ListTodosNotDone,
-    'todo-form-add': FormTodoAdd
+    'todo-form-add': FormTodoAdd,
+    'table-app': TableApp
   },
   methods: {
     redirectToAjouterTodo () {
@@ -57,6 +74,8 @@ export default defineComponent({
       this.todosDone = response.data.filter(todo => todo.done === true)
       this.todosNotDone = response.data.filter(todo => todo.done === false)
       this.loading = false
+      console.table(this.todosDone)
+      console.table(this.todosDone)
     } catch (error) {
       console.log(error)
     }
