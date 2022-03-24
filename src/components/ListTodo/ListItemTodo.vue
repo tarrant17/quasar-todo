21cbe77d5835075a5abcf00446ef46b7a58d2b6c
renab <renaudabadie@gmail.com>
2022-03-24 09:27:03 +0100

    composition api

--- src/components/ListTodo/ListItemTodo.vue
+++ src/components/ListTodo/ListItemTodo.vue
@@ -7,57 +7,49 @@
     <q-item-section side right class="text-white">
       <div>
         {{formattedDueDate}}
-        <q-btn @click.stop="this.showDialogEditTodo = true" flat class="q-ml-md" icon="edit" />
+        <q-btn @click.stop="showDialogEditTodo = true" flat class="q-ml-md" icon="edit" />
         <q-btn @click.stop="deleteTodo" flat class="q-ml-md" icon="delete" />
       </div>
     </q-item-section>
   </q-item>
    <q-dialog v-model="showDialogEditTodo">
       <q-card class="q-pa-xl">
-        <todo-form-edit :todo="todo" @cancel="this.showDialogEditTodo = false" />
+        <todo-form-edit :todo="todo" @cancel="showDialogEditTodo = false" />
       </q-card>
     </q-dialog>
 </template>
 <script>
-import { defineComponent } from 'vue'
-import { mapActions } from 'vueX'
+import { ref, computed } from 'vue'
 import { date } from 'quasar'
 import FormTodoEdit from '../FormTodo/FormTodoEdit'
+import { useStore } from 'vuex'
 
-export default defineComponent({
-  name: 'todo-item',
+export default {
   props: ['todo', 'bgColor'],
-  emits: ['editTodo'],
-  data () {
-    return {
-      showDialogEditTodo: false
-    }
-  },
   components: {
     'todo-form-edit': FormTodoEdit
   },
-  methods: {
-    ...mapActions({ deleteTodoAction: 'deleteTodo', toggleTodoDoneAction: 'toggleTodoDone' }),
-    toggleTaskDone () {
-      this.toggleTodoDoneAction({ id: this.todo.id, updates: { done: !this.todo.done } })
-    },
-    editTodo () {
-      console.log('editTodo', this.todo.id)
-    },
-    deleteTodo () {
-      this.deleteTodoAction({ id: this.todo.id })
+
+  // composition API
+  setup (props, context) {
+    const store = useStore()
+    const showDialogEditTodo = ref(false)
+    function toggleTaskDone () {
+      store.dispatch('toggleTodoDone', { id: props.todo.id, updates: { done: !props.todo.done } })
     }
-  },
-  computed: {
-    taskDone: {
-      get () { return this.todo.done },
-      set () { return this.toggleTaskDone() }
-    },
-    formattedDueDate () {
-      if (this.todo.dueDate == null) return ''
-      const dateJS = date.extractDate(this.todo.dueDate, 'YYYY/MM/DD')
+    function deleteTodo () {
+      store.dispatch('deleteTodo', { id: props.todo.id })
+    }
+    const formattedDueDate = computed(() => {
+      if (props.todo.dueDate == null) return ''
+      const dateJS = date.extractDate(props.todo.dueDate, 'YYYY/MM/DD')
       return date.formatDate(dateJS, 'DD/MM/YYYY')
-    }
+    })
+    const taskDone = computed({
+      get: () => { return props.todo.done },
+      set: () => { return toggleTaskDone() }
+    })
+    return { showDialogEditTodo, toggleTaskDone, deleteTodo, formattedDueDate, taskDone }
   }
-})
+}
 </script>
